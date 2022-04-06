1934b06484e39494c7aaa40bf62482944a890ed3
mheard100 <mjamheard@gmail.com>
2022-04-06 17:23:38 -0500

    added mapping points

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+//let map = L.map('mapid').setView([40.7, -94.5], 4);
 
 //In the code block above:
 //We're assigning the variable map to the object L.map(), and we'll instantiate the object with the given string 'mapid'.
@@ -19,12 +19,33 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //  zoom: 4
 //});
 
+
+
+//Changing map object to focus on LA with a zoom of 14 and updated coordinates
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
+//  Add a marker to the map for Los Angeles, California.
+//let marker = L.marker([34.0522, -118.2437]).addTo(map);
+//Changing marker to a circle
+//L.circle([34.0522, -118.2437], {
+//    radius: 100
+// }).addTo(map);
+//Alternately we can use circle marker function. (Can't get this to work. Works with the above code. The fill color is the reason)
+L.circleMarker([34.0522, -118.2437], {
+   radius: 300,
+   color: "black,"
+   fillColor: "#ffffa1"
+}).addTo(map);
+
+
 // We create the tile layer that will be the background of our map.
 //'https://api.mapbox.com/styles/v1/{id}/ was replaced in the tile layer with Static Tiles API. 
 //Then Removed the .png from the URL. Then Remove the id attribute and the map style reference.
 
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+// We create the tile layer that will be the background of our map.
+
+//Changed the tilelayer style for this index compared to first.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
