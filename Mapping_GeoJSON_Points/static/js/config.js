4145702ac394a4da8666f8cbd807b9742de7921f
mheard100 <mjamheard@gmail.com>
2022-04-09 13:59:11 -0500

    GeoPoints

---
+++ Mapping_GeoJSON_Points/static/js/config.js
@@ -0,0 +1,9 @@
+// API Key
+const API_KEY = "pk.eyJ1IjoibWhlYXJkMTAwIiwiYSI6ImNsMWJhZ3M2eTJld3QzY3AzeDYyZ2xtdzgifQ.Vn9CC3n5wfEyH8otRCdlyg";
+
+
+
+//To open the index.html file in the browser:
+
+//Open the command line and navigate to your Mapping_Earthquakes folder. The index.html file should be in the top-level of that folder.
+//On the command line, type python -m http.server and press Enter.
